3134e3469297ee3c85910ab9ae9c2282577433d6
sebabouche <s.nicolaidis@me.com>
2016-06-17 13:12:44 +0200

    Model creation, OneToOne relations, signals and the Django Admin

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taskbuster.apps.taskmanager',
 ]
 
 MIDDLEWARE_CLASSES = [
